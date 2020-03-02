a8cc5dcbde3d01c97e29c39b70fa6883f77e7c7f
JoaoCostaIFG <joaocosta.tra@outlook.com>
2020-03-02 01:06:06 +0000

    Improving keyboard writting code.

--- src/keyboard/piz_hid-keyboard.c
+++ src/keyboard/piz_hid-keyboard.c
@@ -1,8 +1,9 @@
-/* This program convers strings/chars given in argv to the code used by keyboards on keypress */
-/* Can be used, for example, on a raspberry pi that is acting as an HID gadget
- * (FSconfig) to act as a keyboard on a connected device (through USB OTG)
+/*
+ * This program convers strings/chars given in argv to the code used by
+ * keyboards on keypress Can be used, for example, on a raspberry pi that is
+ * acting as an HID gadget (FSconfig) to act as a keyboard on a connected device
+ * (with USB OTG)
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,52 +15,54 @@
 #define _XOPEN_SOURCE 700
 
 void sleep_ms(int milliseconds) {
-	/* pause execution for milliseconds ms */
-	struct timespec ts;
+  /* pause execution for milliseconds ms */
+  struct timespec ts;
 
-	ts.tv_sec = milliseconds / 1000;
-	ts.tv_nsec = (milliseconds % 1000) * 1000000;
-	nanosleep(&ts, NULL);
+  ts.tv_sec = milliseconds / 1000;
+  ts.tv_nsec = (milliseconds % 1000) * 1000000;
+  nanosleep(&ts, NULL);
 }
 
-
-void release_keys(FILE *fp){
-	fprintf(fp, "%c%c%c%c%c%c%c%c", '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0');
+void release_keys(FILE *fp) {
+  fprintf(fp,"%c%c%c%c%c%c%c%c", '\0', '\0', '\0', '\0', '\0', '\0', '\0',
+          '\0');
 }
 
+void write_key(char modifier, char keytowrite, FILE *fp) {
+  /* press key */
+  fprintf(fp, "%c%c%c%c%c%c%c%c", modifier, '\0', keytowrite, '\0', '\0', '\0',
+          '\0', '\0');
 
-void write_key(char modifier, char keytowrite, FILE *fp){
-	/* press key */
-	fprintf(fp, "%c%c%c%c%c%c%c%c", modifier, '\0', keytowrite, '\0', '\0', '\0', '\0', '\0');
-
-	/* release key */
-	release_keys(fp);
+  /* release key */
+  /* release_keys(fp); */
 }
 
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    fputs("Usage: piz_hid-keyboard <layout> <text>\n", stderr);
+    exit(1);
+  }
 
-int main(int argc, char *argv[]){
-	if (argc < 3) {
-		fprintf(stderr, "Usage: piz_hid-keyboard <layout> <text>");
-		exit (1);
-	}
+  int i, j; // for loop indexes
+  short unsigned int lay = atoi(argv[1]);
+  FILE *fp;
+  fp = fopen("/dev/hidg0", "w");
 
-	int i, j; //for loop indexes
-        short unsigned int lay = atoi(argv[1]);
-	FILE *fp;
-        fp = fopen("/dev/hidg0", "w");
+  for (i = 2; i < argc; i++) {
+    for (j = 0; j < strlen(argv[i]); j++) {
+      write_key(layout[argv[i][j]].keys[lay].modifier,
+                layout[argv[i][j]].keys[lay].keytowrite, fp);
+      if (layout[argv[i][j]].keys[lay].is_dead)
+        write_key(layout[argv[i][j]].keys[lay].modifier,
+                  layout[argv[i][j]].keys[lay].keytowrite, fp);
+    }
 
-	for (i = 2; i < argc; i++) {
-		for (j = 0; j < strlen(argv[i]); j++){
-			write_key(layout[argv[i][j]].keys[lay].modifier, layout[argv[i][j]].keys[lay].keytowrite, fp);
-			if (layout[argv[i][j]].keys[lay].is_dead)
-				write_key(layout[argv[i][j]].keys[lay].modifier, layout[argv[i][j]].keys[lay].keytowrite, fp);
-		}
+    if (i < argc - 2)
+      write_key(layout[' '].keys[lay].modifier,
+                layout[' '].keys[lay].keytowrite, fp); // white space
+  }
 
-		if (i < argc - 2)
-			write_key(layout[' '].keys[lay].modifier, layout[' '].keys[lay].keytowrite, fp); //white space
-
-	}
-
-	fclose(fp);
-        return 0;
+  release_keys(fp);
+  fclose(fp);
+  return 0;
 }
